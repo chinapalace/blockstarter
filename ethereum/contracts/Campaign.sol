4f290a7ee5c3f04058082977251e70a60f0879b5
Justin McLaren <justinmclaren63@gmail.com>
2018-04-03 14:55:26 -0700

    tests complete and passing

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -2,12 +2,12 @@ pragma solidity ^0.4.17;
 
 contract CampaignFactory {
     address[] public deployedCampaigns;
-    
+
     function createCampaign(uint minimum) public {
-      address newCampaign =  new Campaign(minimum);
-      deployedCampaigns.push(newCampaign);
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
     }
-    
+
     function getDeployedCampaigns() public view returns (address[]) {
         return deployedCampaigns;
     }
@@ -22,32 +22,31 @@ contract Campaign {
         uint approvalCount;
         mapping(address => bool) approvals;
     }
-    
+
     Request[] public requests;
     address public manager;
     uint public minimumContribution;
     mapping(address => bool) public approvers;
     uint public approversCount;
-    
+
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
-    
-    function Campaign(uint minimum) public {
-        manager = msg.sender;
+
+    function Campaign(uint minimum, address creator) public {
+        manager = creator;
         minimumContribution = minimum;
     }
-    
+
     function contribute() public payable {
         require(msg.value > minimumContribution);
-        
+
         approvers[msg.sender] = true;
         approversCount++;
     }
-    
-    function createRequest(string description, uint value, address recipient) 
-        public restricted {
+
+    function createRequest(string description, uint value, address recipient) public restricted {
         Request memory newRequest = Request({
            description: description,
            value: value,
@@ -55,27 +54,43 @@ contract Campaign {
            complete: false,
            approvalCount: 0
         });
-        
+
         requests.push(newRequest);
     }
-    
+
     function approveRequest(uint index) public {
         Request storage request = requests[index];
-        
+
         require(approvers[msg.sender]);
         require(!request.approvals[msg.sender]);
-        
+
         request.approvals[msg.sender] = true;
         request.approvalCount++;
     }
-    
+
     function finalizeRequest(uint index) public restricted {
         Request storage request = requests[index];
-        
+
         require(request.approvalCount > (approversCount / 2));
         require(!request.complete);
-        
+
         request.recipient.transfer(request.value);
         request.complete = true;
+    }
+
+    function getSummary() public view returns (
+      uint, uint, uint, uint, address
+      ) {
+        return (
+          minimumContribution,
+          this.balance,
+          requests.length,
+          approversCount,
+          manager
+        );
+    }
+
+    function getRequestsCount() public view returns (uint) {
+        return requests.length;
     }
 }
